d20dd8c01d412097ec0f2f75cd83f96404399ef1
Achiariello9 <a.chiariello9@studenti.uniba.it>
2023-05-31 20:05:19 +0200

    da completare Kmeans2

--- src/data/Item.java
+++ src/data/Item.java
@@ -1,5 +1,30 @@
 package data;
+import utility.ArraySet;
 
-class Item {
+abstract class Item {
+	Attribute attribute;
+	Object value;
+	//costruttore con parametri
+	Item(Attribute attribute,Object value) {
+		this.attribute = attribute;
+		this.value= value;
+	}
+	public Attribute getAttribute() {
+		return attribute;
+	}
+	public Object getValue() {
+		return value;
+	}
+	@Override
+	public String toString() {
+		return "Item [value=" + value + "]";
+	}
+	abstract double distance(Object a);
+
+	void update(Data data, ArraySet clusteredData){
+		value =  data.computePrototype(clusteredData,attribute);
+	}
+
 
 }
+
