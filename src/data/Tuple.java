afd0342f75ad1597db48056d0acd170f5beb1a86
nicola1997 <n.dipinto11@gmail.com>
2023-06-03 19:43:17 +0200

    add tutto

--- src/data/Tuple.java
+++ src/data/Tuple.java
@@ -14,24 +14,23 @@ public class Tuple {
 	public Item get(int i) {
 		return tuple[i];
 	}
-	void add(Item c,int i) {
-		tuple[i]=c;
-	}
-	
-	double getDistance(Tuple obj) { //da fare 
-		return 0;
-		
+
+	void add(Item c, int i) {
+		tuple[i] = c;
 	}
 
+	double getDistance(Tuple obj) { // da fare
+		return 0;
+
+	}
 
 	public void setTuple(Item[] tuple) {
 		this.tuple = tuple;
 	}
 
 	public String avgDistance(Data data, int[] array) {
-		// TODO Auto-generated method stub
-		return null;
-	} 
 
-	
+		return null;
+	}
+
 }
