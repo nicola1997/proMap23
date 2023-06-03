5c29186e7af81b4a653ccbade7779a87c2d0ef44
nicola1997 <n.dipinto11@gmail.com>
2023-06-03 13:37:38 +0200

    aggiorna Data ecc

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,6 +1,6 @@
-import data.Data;
 import data.Attribute;
 import data.DiscreteAttribute;
+import data.ContinuousAttribute;
 
 public class MainTest {
 
@@ -9,23 +9,24 @@ public class MainTest {
 	 */
 	public static void main(String[] args) {
 		String[] Values;
-		Values= new String[10];
-		Attribute a= new DiscreteAttribute("sunny",7,Values); 
+		Values = new String[10];
+		Attribute a = new DiscreteAttribute("sunny", 7, Values);
+
+		/*
+		 * Data data =new Data(); String[] Values1; Values1= new String[10]; Attribute
+		 * a1= new DiscreteAttribute("sunny",7,Values1); System.out.println();
+		 * System.out.println(data); int k=3;
+		 */
+		/*
+		 * KmeansMiner kmeans=new KmeansMiner(k); int numIter=kmeans.kmeans(data);
+		 * System.out.println("Numero di Iterazione:"+numIter);
+		 * System.out.println(kmeans.getC().toString(data));
+		 */
+
+		ContinuousAttribute a1 = new ContinuousAttribute("Eta",3,10,20);
 		
-		
-		Data data =new Data();
-		String[] Values;
-		Values= new String[10];
-		Attribute a= new DiscreteAttribute("sunny",7,Values);
-		System.out.println();
-		System.out.println(data);
-		int k=3;
-		/*KmeansMiner kmeans=new KmeansMiner(k);
-		int numIter=kmeans.kmeans(data);
-		System.out.println("Numero di Iterazione:"+numIter);
-		System.out.println(kmeans.getC().toString(data));*/
-		
-		
+		System.out.print(a1.getScaledValue(15));
+
 	}
 
 }
